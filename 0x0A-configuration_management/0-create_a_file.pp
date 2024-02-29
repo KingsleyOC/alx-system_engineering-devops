3cc729a062e5f9ec5adc8625f6623ac9df5494f5
KingsleyOC <kingsleyokechukwu890@gmail.com>
2024-02-28 19:55:29 -0800

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Here, the Puppet manifest creates a file in /tmp
+
+file { 'holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Here, the Puppet manifest kills a process named killmenow
+
+exec { 'process kill killmenow':
+  path    => '/usr/bin/',
+  command => 'pkill -f killmenow',
+}
